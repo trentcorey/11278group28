61be3b84092ac45c10b2e97be18f8de63c9ba1ac
OverlordFlowers <chenruo@ufl.edu>
2021-10-24 20:12:37 -0400

    Added image sectioning functionality

--- CNN/CNN_Proj_Utils/util_image_read.py
+++ CNN/CNN_Proj_Utils/util_image_read.py
@@ -15,17 +15,29 @@ def annotate_image(filepath, image_name, image_class):
 
     # print('Total number of contours detected: ' + str(len(contours)))
 
-    # Draw a bounding box around all contours
+    # for c in contours:
+    #     x, y, w, h = cv2.boundingRect(c)
+    #     annotation = 0
+
+    #     # Make sure contour area is large enough
+    #     if (cv2.contourArea(c)) > contour_area_percent * im_hei * im_wid:
+    #         print(cv2.contourArea(c))
+    #         cv2.rectangle(with_contours,(x,y), (x+w,y+h), (255,0,0), 5)
+    #         # annotation = filepath + " " + str(x/im_wid) + " " + str(y/im_hei) + " " + str(w/im_wid) + " " + str(h/im_hei) + str(image_class) + "\n"
+    #         annotation = filepath + " " + str(x) + "," + str(y) + "," + str(w + x) + "," + str(h + y) + "," + str(image_class) + "\n"
+    #         cv2.imwrite("bounding_boxes\\" + image_name, with_contours)
+
+    contour_areas = []
     for c in contours:
-        x, y, w, h = cv2.boundingRect(c)
+        contour_areas.append(cv2.contourArea(c))
 
-        # Make sure contour area is large enough
-        if (cv2.contourArea(c)) > 10000:
-            cv2.rectangle(with_contours,(x,y), (x+w,y+h), (255,0,0), 5)
-            # annotation = filepath + " " + str(x/im_wid) + " " + str(y/im_hei) + " " + str(w/im_wid) + " " + str(h/im_hei) + str(image_class) + "\n"
-            annotation = filepath + " " + str(x) + "," + str(y) + "," + str(w + x) + "," + str(h + y) + "," + str(image_class) + "\n"
-
-    # # Show image    
+    max_area = max(contour_areas)
+    max_index = contour_areas.index(max_area)
+    x, y, w, h = cv2.boundingRect(contours[max_index])
+    annotation = filepath + " " + str(x) + "," + str(y) + "," + str(w + x) + "," + str(h + y) + "," + str(image_class) + "\n"
+    cv2.rectangle(with_contours,(x,y), (x+w,y+h), (255,0,0), 5)
+    cv2.imwrite("bounding_boxes\\" + image_name, with_contours)
+    # Show image    
     # cv2.imshow('Bounding Box', with_contours)
     # cv2.waitKey(0)
     # cv2.destroyAllWindows()
--- CNN/__main__.py
+++ CNN/__main__.py
@@ -5,4 +5,12 @@ import os
 if __name__ == "__main__":
     # Check to make sure directories exist. If they don't, create them.
     # util_directory_check.directory_check()
-    util_process_image.process_image("images\\apple1.jpg", "apple")
+    # util_process_image.process_image("images\\apple1.jpg", "apple")
+    i = 0
+    directory = r'images' # Sets directory to images
+    for entry in os.scandir(directory):
+        if (entry.path.endswith(".jpg") and entry.is_file()):
+            util_process_image.process_image(entry.path, "apple")
+            i = i+1
+
+    print("Processed %d images.\n", i)
