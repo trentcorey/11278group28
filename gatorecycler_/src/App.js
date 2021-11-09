38601556284bdd677c5bcbaf3d82587822715cbc
Samantha Martinez <martinezs@ufl.edu>
2021-11-09 16:35:20 -0500

    Fixing App.js Error
    It was displaying the homepages incorrectly because or error in App.js, but its fixed now.

--- gatorecycler_/src/App.js
+++ gatorecycler_/src/App.js
@@ -6,9 +6,6 @@ import Home from './Pages/Home';
 import UploadItems from './Pages/UploadItem';
 import {HashRouter as Router, Route} from 'react-router-dom';
 import SignIn from './Pages/SignIn';
-import About from './Pages/About';
-import Resources from './Pages/Resources';
-import Help from './Pages/Help';
 
 function App() {
   return (
@@ -20,9 +17,6 @@ function App() {
         <Route exact path = '/sign-in'><SignIn/></Route>
         <Route exact path= '/'><Home/></Route>
         <Route exact path='/upload-image'><UploadItems/></Route>
-        <Route exact path= '/about'><About/></Route>
-        <Route exact path= '/resources'><Resources/></Route>
-        <Route exact path= '/help'><Help/></Route>
         <Route exact path= '/about'></Route>
         <Route exact path= '/resources'></Route>
         <Route exact path= '/help'></Route>
