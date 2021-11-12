541a838d707625dd430014761c22cc8f6ef19339
OverlordFlowers <chenruo@ufl.edu>
2021-11-12 16:14:10 -0500

    This is now empty

--- README.md
+++
@@ -1,6 +0,0 @@
-# -11278group28
-
-Member1: Trent Corey
-Member2: Ruo Chen
-Member3: Samantha Martinez
-Member4: Shaddai Ensley
--- backend/python_helpers/delete_result.py
+++
@@ -1,3 +0,0 @@
-import os
-# This deletes the result.jpg on file change so IT STOPS DISPLAYING.
-os.remove("uploads/result.jpg")
